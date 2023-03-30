80222b4ab5dd8cc8e89b8fc9fe0e2c43fac8f55f
Ilyass Baba <ilyassbaba.me@gmail.com>
2023-03-30 04:42:29 +0000

    Clean code and working Departure / Arrival Btns

--- src/Component/Content/index.js
+++ src/Component/Content/index.js
@@ -7,27 +7,54 @@ import "./index.css"
 
 function Content(props) {
 
-    
-  const [metar, setMetar] = useState("");
+  // Hooks to pick up data
+
+  const [metar, setMetar] = useState("")
   const [icaoCode, setIcaoCode] = useState("");
 
+
+  // Function to get the icao from input
   const handleInputChange = (event) => {
     setIcaoCode(event.target.value);
   };
 
-  const fetchMetar = () => {
-    if(icaoCode != ""){
-      
-    let url = "http://localhost:8000/" + icaoCode;
-    fetch(url)
-      .then(response => response.text())
-      .then(data => setMetar(data))
-      .catch(error => console.error(error));
-    }
-    else{
-      setMetar("")
-    }
-  };
+
+
+  // Function to fetch origin icao code from Simbrief and send it to fetchmetar function
+  function fetchOriginMetar(){
+    fetch("https://www.simbrief.com/api/xml.fetcher.php?username=IlyassBaba&json=1")
+    .then(response => response.json())
+    .then(response => fetchMetar(response.api_params.orig))
+  }
+
+  
+  // Function to fetch destination icao code from Simbrief and send it to fetchmetar function
+  function fetchDestinationMetar(){
+    fetch("https://www.simbrief.com/api/xml.fetcher.php?username=IlyassBaba&json=1")
+    .then(response => response.json())
+    .then(response => fetchMetar(response.api_params.dest))
+  }
+  
+
+  // Function to fetch the data from api using the icao code
+  function fetchMetar(Code) {
+        if(Code != ""){
+          
+          let url = "http://localhost:8000/" + Code;
+          
+          fetch(url)
+            .then(response => response.text())
+            .then(data => setMetar(data))
+            .catch(error => console.error(error));
+        }
+        else{
+          setMetar("")
+        }
+      }
+
+    
+
+
 
 
 
@@ -42,9 +69,9 @@ function Content(props) {
             </div>
             
             <div className="FetchBtn Content-Placement">    
-                <a type="button">Depature</a>
-                <a type="button">Arrival</a>
-                <a type="button" onClick={fetchMetar}>Fetch</a>
+                <a type="button" onClick={() => fetchOriginMetar()}>Depature</a>
+                <a type="button" onClick={() => fetchDestinationMetar()}>Arrival</a>
+                <a type="button" onClick={() => fetchMetar(icaoCode)}>Fetch</a>
             </div>
         </div>
     )
