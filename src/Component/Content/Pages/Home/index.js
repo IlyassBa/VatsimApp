d0ce0a57151b5f2519d81db0a7f180b5c8fe3843
Ilyass Baba <ilyassbaba.me@gmail.com>
2023-04-24 10:07:09 +0100

    working home api

--- src/Component/Content/Pages/Home/index.js
+++ src/Component/Content/Pages/Home/index.js
@@ -17,67 +17,91 @@ function Home(props) {
   // Hooks to set Value for HTMLElements
   const [Username,setUsername] = useState("")
 
-  const [OriginAirportZtime,setOriginAirportZtime] = useState("")
-  const [DirectionAirportZtime,setDirectionAirportZtime] = useState("")
-
+  // Visibility Time
+  const [OriginAirportVisibility,setOriginAirportVisibility] = useState("")
+  const [DirectionAirportVisibility,setDirectionAirportVisibility] = useState("")
+  // Icao codes
   const [OriginAirport,setOriginAirport] = useState("")
   const [DirectionAirport,setDirectionAirport] = useState("")
-
+  // Winds
   const [OriginAirportWinds,setOriginAirportWinds] = useState("")
   const [DirectionAirportWinds,setDirectionAirportWinds] = useState("")
+  // Temperatures
+  const [OriginAirportTemp,setOriginAirportTemp] = useState("")
+  const [DirectionAirportTemp,setDirectionAirportTemp] = useState("")
+  // Pressure
+  const [OriginAirportQNH,setOriginAirportQNH] = useState("")
+  const [DirectionAirportQNH,setDirectionAirportQNH] = useState("")
+  // Sky conditions
+  const [OriginAirportSky,setOriginAirportSky] = useState("")
+  const [DirectionAirportSky,setDirectionAirportSky] = useState("")
 
-
-  // const [OriginAirportWinds,setOriginAirportWinds] = useState("")
-  // const [DirectionAirportWinds,setDirectionAirportWinds] = useState("")
 
 // Function to Fetch Data from Simbrief
 useEffect(() => {
 
-  function FetchSimbrief(){
-    fetch("https://www.simbrief.com/api/xml.fetcher.php?username=" + data.simbriefName + "&json=1")
-    .then((response) => response.json())
+  // function FetchSimbrief(){
+    fetch("https://www.simbrief.com/api/xml.fetcher.php?username= " + data.simbriefName + "&json=1")
+    .then((response) => response.json())  
     .then((response) =>{
-
-
       setOriginAirport(response.api_params.orig)
       setDirectionAirport(response.api_params.dest)
 
       setUsername(response.api_params.orig)
 
-      // Arrays to split Weather Data
-      const WeatherArrayOrigin = response.weather.orig_metar.split(" ")
-      const WeatherArrayDestination= response.weather.dest_metar.split(" ")
 
+    })
+
+    let url = "http://192.168.1.7:8000/" + OriginAirport
+
+    fetch(url)
+    .then((response) => response.json())  
+    .then((response) =>{
+      console.log(OriginAirport)
+      setOriginAirportWinds(response[OriginAirport].wind + " at " + response[OriginAirport].wind_vel + " Kts")
+      setOriginAirportTemp(response[OriginAirport].temp + ' / ' + response[OriginAirport].dewpoint)
+      setOriginAirportSky(response[OriginAirport].sky_conditions[0].coverage)
       
-      setOriginAirportWinds(WeatherArrayOrigin[2])
-      setDirectionAirportWinds(WeatherArrayDestination[2])
-      
-      setOriginAirportZtime(WeatherArrayOrigin[1])
-      setDirectionAirportZtime(WeatherArrayDestination[1])
+      if(response[OriginAirport].sky_conditions[0].coverage == "CLR")
+        setOriginAirportSky("clear sky conditions")
+      if(response[OriginAirport].sky_conditions[0].coverage == "FEW")
+        setOriginAirportSky("few clowds")
+
+      setOriginAirportQNH(response[OriginAirport].alt_hg)
+      setOriginAirportVisibility(response[OriginAirport].visibility)
 
       console.log(response)
 
+    url = "http://192.168.1.7:8000/" + DirectionAirport
 
-      // console.log(WeatherArray)
-    })
-  }
+    fetch(url)
+      .then((response) => response.json())  
+      .then((response) =>{
+        console.log(DirectionAirport)
+        setDirectionAirportWinds(response[DirectionAirport].wind + " Kts at " + response[DirectionAirport].wind_vel + " Kts")
+        setDirectionAirportTemp(response[DirectionAirport].temp + ' / ' + response[DirectionAirport].dewpoint)
+        
+        if(response[DirectionAirport].sky_conditions[0].coverage == "CLR")
+          setDirectionAirportSky("clear sky conditions")
+        if(response[DirectionAirport].sky_conditions[0].coverage == "FEW")
+          setDirectionAirportSky("few clowds")
+        
+        setDirectionAirportQNH(response[DirectionAirport].alt_hg)
+        setDirectionAirportVisibility(response[DirectionAirport].visibility)
+        console.log(response)
+    
+      })
+    
 
-
-  setInterval(() => {
-    FetchSimbrief()
-  }, 1000);
-  
-
-
-},[])
-
-
+}
+,[])
+})
 
     return (
         <div className="homeContent">
           <div className="Welcome-Container animate__fadeInLeft"> 
             <div><p>Welcome {data.simbriefName},</p></div>
-            <div><p>Do you want to Start your flight from {OriginAirport} to {DirectionAirport} , I Saw its gonna be cloudy on the departure</p></div>
+            <div><p>Do you want to Start your flight from {OriginAirport} to {DirectionAirport} , I Saw its gonna be {OriginAirportSky} on the departure and {DirectionAirportSky} on arrival</p></div>
             <div><p className="Welcome-Container-link">Start The Flight</p></div>
           </div>
 
@@ -85,10 +109,10 @@ useEffect(() => {
             <div className="Weather-Container-Title Weather-Container-Margin"><p>Departure</p></div>
             <div className="Weather-Container-Content">
 
-              <WeatherInfo icon="fa-solid fa-clock" label={OriginAirportZtime}></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-eye" label={OriginAirportVisibility}></WeatherInfo>
               <WeatherInfo icon="fa-solid fa-wind" label={OriginAirportWinds}></WeatherInfo>
-              <WeatherInfo icon="fa-solid fa-temperature-three-quarters" label="Temp"></WeatherInfo>
-              <WeatherInfo icon="fa-solid fa-gauge" label="1013"></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-temperature-three-quarters" label={OriginAirportTemp}></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-gauge" label={OriginAirportQNH}></WeatherInfo>
              
             </div>
           </div>
@@ -97,16 +121,16 @@ useEffect(() => {
             <div className="Weather-Container-Title Weather-Container-Margin"><p>Arrival</p></div>
             <div className="Weather-Container-Content">
 
-              <WeatherInfo icon="fa-solid fa-clock" label={DirectionAirportZtime}></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-eye" label={DirectionAirportVisibility}></WeatherInfo>
               <WeatherInfo icon="fa-solid fa-wind" label={DirectionAirportWinds}></WeatherInfo>
-              <WeatherInfo icon="fa-solid fa-temperature-three-quarters" label="Temp"></WeatherInfo>
-              <WeatherInfo icon="fa-solid fa-gauge" label="1013"></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-temperature-three-quarters" label={DirectionAirportTemp}></WeatherInfo>
+              <WeatherInfo icon="fa-solid fa-gauge" label={DirectionAirportQNH}></WeatherInfo>
              
             </div>
           </div>
           
         </div>
-    )
-}
+    )}
+
 
 export default Home;
